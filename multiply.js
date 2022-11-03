47669f939593274f4115b1039a0e4fc53438b91b
ykgw-sindhu <113009736+ykgw-sindhu@users.noreply.github.com>
2022-11-03 09:02:05 +0530

    Update multiply.js

--- multiply.js
+++ multiply.js
@@ -2,3 +2,14 @@ function myFunction
 (p1, p2) {
   return p1 * p2; // The function returns the product of p1 and p2
 }
+
+
+function product(a, b) {
+        return a * b;
+    }
+  
+    // Calling product() function
+    var result = product.call(this, 20, 5);
+  
+    document.write(result);
+</script>
